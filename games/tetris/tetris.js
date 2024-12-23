c55bf672ea768d0aef46d55f2bf027260838be33
BubsterRoo <ajbraden826@gmail.com>
2024-12-23 16:28:24 -0500

    Add files via upload

--- games/tetris/tetris.js
+++ games/tetris/tetris.js
@@ -1,34 +1,84 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const board = document.getElementById('board');
+const boardctx = board.getContext('2d');
+const nextPiece = document.getElementById('nextPiece');
+const nextPiecectx = nextPiece.getContext('2d');
+const hold = document.getElementById('hold');
+const holdctx = hold.getContext('2d');
+const score = document.getElementById('score');
+const scorectx = score.getContext('2d');
 
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+board.width = board.offsetWidth;
+board.height = board.offsetHeight;
+nextPiece.width = nextPiece.offsetWidth;
+nextPiece.height = nextPiece.offsetHeight;
+hold.width = hold.offsetWidth;
+hold.height = hold.offsetHeight;
+score.width = score.offsetWidth;
+score.height = score.offsetHeight;
 
-const floorImage = new Image();
-floorImage.src = 'images/I-piece.png';
-const floorImage = new Image();
-floorImage.src = 'images/L-piece.png';
-const floorImage = new Image();
-floorImage.src = 'images/J-piece.png';
-const floorImage = new Image();
-floorImage.src = 'images/Z-piece.png';
-const floorImage = new Image();
-floorImage.src = 'images/S-piece.png';
-const floorImage = new Image();
-floorImage.src = 'images/O-piece.png';
-const floorImage = new Image();
-floorImage.src = 'images/T-piece.png';
-const floorImage = new Image();
-floorImage.src = 'images/I-piece-outline.png';
-const floorImage = new Image();
-floorImage.src = 'images/L-piece-outline.png';
-const floorImage = new Image();
-floorImage.src = 'images/J-piece-outline.png';
-const floorImage = new Image();
-floorImage.src = 'images/Z-piece-outline.png';
-const floorImage = new Image();
-floorImage.src = 'images/S-piece-outline.png';
-const floorImage = new Image();
-floorImage.src = 'images/O-piece-outline.png';
-const floorImage = new Image();
-floorImage.src = 'images/T-piece-outline.png';
+const iPiece = new Image();
+iPiece.src = 'images/I-piece.png';
+const lPiece = new Image();
+lPiece.src = 'images/L-piece.png';
+const jPiece = new Image();
+jPiece.src = 'images/J-piece.png';
+const zPiece = new Image();
+zPiece.src = 'images/Z-piece.png';
+const sPiece = new Image();
+sPiece.src = 'images/S-piece.png';
+const oPiece = new Image();
+oPiece.src = 'images/O-piece.png';
+const tPiece = new Image();
+tPiece.src = 'images/T-piece.png';
+const iPieceOutline = new Image();
+iPieceOutline.src = 'images/I-piece-outline.png';
+const lPieceOutline = new Image();
+lPieceOutline.src = 'images/L-piece-outline.png';
+const jPieceOutline = new Image();
+jPieceOutline.src = 'images/J-piece-outline.png';
+const zPieceOutline = new Image();
+zPieceOutline.src = 'images/Z-piece-outline.png';
+const sPieceOutline = new Image();
+sPieceOutline.src = 'images/S-piece-outline.png';
+const oPieceOutline = new Image();
+oPieceOutline.src = 'images/O-piece-outline.png';
+const tPieceOutline = new Image();
+tPieceOutline.src = 'images/T-piece-outline.png';
+const background = new Image();
+background.src = 'images/Background.png';
+
+const nextText = "NEXT";
+nextPiecectx.font = '30px Arial';
+nextPiecectx.fillStyle = "white";
+const nextTextWidth = nextPiecectx.measureText(nextText).width;
+nextPiecectx.fillText(nextText, (nextPiece.width - nextTextWidth) / 2, 35);
+
+const holdText = "HOLD";
+holdctx.font = '30px Arial';
+holdctx.fillStyle = "white";
+const holdTextWidth = holdctx.measureText(holdText).width;
+holdctx.fillText(holdText, (hold.width - holdTextWidth) / 2, 35);
+
+const scoreText = "|||||||||||||||||||||||||||||||||||||||||||||";
+scorectx.font = '30px Arial';
+scorectx.fillStyle = "white";
+const scoreTextWidth = scorectx.measureText(scoreText).width;
+scorectx.fillText(scoreText, (score.width - scoreTextWidth) / 2, 35);
+
+
+
+let xPos = 0;
+let yPos = 0;
+const backgroundSize = (board.width - 9) / 10;
+
+background.onload = function(){
+    drawBackground();
+}
+
+function drawBackground(){
+    for(let i = 0; i < 10; i++){
+        for(let j = 0; j < 20; j++){
+            boardctx.drawImage(background, xPos + (i * (backgroundSize + 1)), yPos + (j * (backgroundSize + 1)), backgroundSize, backgroundSize);
+        }
+    }
+}
